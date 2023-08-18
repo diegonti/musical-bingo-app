c7b255b3f92216f22b21d99c55c249d6b6e48075
diegonti <diegontidoc2000@gmail.com>
2023-08-18 17:47:24 +0200

    integrated Pathlib

--- app.py
+++ app.py
@@ -1,19 +1,21 @@
 from flask import Flask, render_template, request, send_file, url_for
+from pathlib import Path
 import os
 import shutil
 import random
 import musical_bingo
-# import your_image_generation_script
+
+THIS_FOLDER = Path(__file__).parent.resolve()
 
 def create_upload_dirs(folder,seed):
     
-    try: os.mkdir("uploads")
+    try: os.mkdir(THIS_FOLDER / "uploads")
     except FileExistsError: pass
 
-    try: os.mkdir(f"uploads/{folder}/")
+    try: os.mkdir(THIS_FOLDER / f"uploads/{folder}/")
     except FileExistsError: pass
 
-    try: os.mkdir(f"uploads/{folder}/{seed}")
+    try: os.mkdir(THIS_FOLDER / f"uploads/{folder}/{seed}")
     except FileExistsError: pass
 
 
@@ -21,7 +23,7 @@ def store_upload_images(uploaded_images,out_folder):
     # Store uploaded images temporarily
     image_paths = []
     for image in uploaded_images:
-        image_path = out_folder + image.filename
+        image_path = out_folder / image.filename
         image.save(image_path)
         image_paths.append(image_path)
     return image_paths
@@ -29,7 +31,7 @@ def store_upload_images(uploaded_images,out_folder):
 def store_upload_songs(uploaded_songs,out_folder):
     # Store uploaded songs temporarily
     uploaded_songs = uploaded_songs[0]
-    uploaded_songs_path = out_folder + "songs.txt"
+    uploaded_songs_path = out_folder / "songs.txt"
     uploaded_songs.save(uploaded_songs_path)
     return uploaded_songs_path
 
@@ -64,11 +66,12 @@ def generate_images():
     parameters = request.form.copy()
     parameters = parse_parameters(parameters)
 
-    IMG_FOLDER = f"uploads/img/{user_seed}/"
-    SONGS_FOLDER = f"uploads/songs/{user_seed}/"
+    IMG_FOLDER = THIS_FOLDER / f"./uploads/img/{user_seed}/"
+    SONGS_FOLDER = THIS_FOLDER / f"./uploads/songs/{user_seed}/"
 
-    parameters["IMG_FOLDER"] = IMG_FOLDER
-    parameters["SONGS_FILE"] = SONGS_FOLDER + "songs.txt"
+    parameters["IMG_FOLDER"] = IMG_FOLDER.as_posix()
+    parameters["SONGS_FILE"] = (SONGS_FOLDER / "songs.txt").as_posix()
+    parameters["OUTPUT_FOLDER"] = (THIS_FOLDER / "bills/").as_posix()
 
     # Store images
     create_upload_dirs("img",user_seed)
@@ -80,17 +83,17 @@ def generate_images():
     create_upload_dirs("songs",user_seed)
     if not uploaded_songs[0].filename == "":
         store_upload_songs(uploaded_songs, SONGS_FOLDER)
-    else: parameters["SONGS_FILE"] = "./static/numbers.txt"
+    else: parameters["SONGS_FILE"] = THIS_FOLDER / "./static/numbers.txt"
 
     # Call your image generation script with image_paths
     musical_bingo.main(parameters)
-    shutil.make_archive("bills","zip","bills/")
+    shutil.make_archive(THIS_FOLDER / "bills","zip",THIS_FOLDER / "bills/")
 
     shutil.rmtree(IMG_FOLDER)
     shutil.rmtree(SONGS_FOLDER)
-    shutil.rmtree("bills/")
+    shutil.rmtree(THIS_FOLDER / "bills/")
 
-    return send_file("bills.zip", as_attachment=True, download_name="bills.zip")
+    return send_file(THIS_FOLDER / "bills.zip", as_attachment=True, download_name="bills.zip")
     # return render_template('result.html', images=generated_images_paths)
     # return render_template('results.html')
 
